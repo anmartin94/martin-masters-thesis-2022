f64b1faf515c79bdd6056f140361b898a3324a81
anmartin94 <43837334+anmartin94@users.noreply.github.com>
2022-06-13 18:27:00 -0400

    Update install.sh

--- shared-task-classification-project/install.sh
+++ shared-task-classification-project/install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 wget https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M-subword.zip
 unzip crawl-300d-2M-subword.zip
+
+pip3 install upgrade --pip 
+
+pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+
+pip3 install keras && numpy && sklearn && pandas && tensorflow && seaborn && fasttext && matplotlib && tqdm && xgboost
